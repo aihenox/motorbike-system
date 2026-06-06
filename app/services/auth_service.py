37cd28580b8911756dd37fd41525435067a91d46
Lucho <luisfermh212@gmail.com>
2026-06-06 17:15:53 -0500

    Refactor auth service

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -3,37 +3,31 @@ import os
 from flask_login import UserMixin
 
 from werkzeug.security import (
-
     generate_password_hash,
-
     check_password_hash
 )
 
 from app.repositories.auth_repository import (
-
     obtener_usuario_por_username,
-
     crear_usuario
 )
 
 
 # ==========================================
-# USER LOGIN
+# USUARIO FLASK-LOGIN
 # ==========================================
 class User(UserMixin):
 
     def __init__(self, user_id):
 
-        self.id = user_id
+        self.id = str(user_id)
 
 
 # ==========================================
 # VALIDAR LOGIN
 # ==========================================
 def validar_login(
-
     usuario,
-
     password
 ):
 
@@ -45,12 +39,8 @@ def validar_login(
 
         return None
 
-    password_hash = user["password"]
-
     if not check_password_hash(
-
-        password_hash,
-
+        user["password"],
         password
     ):
 
@@ -60,30 +50,43 @@ def validar_login(
 
 
 # ==========================================
-# CREAR ADMIN DEFAULT
+# GENERAR HASH
+# ==========================================
+def generar_hash_password(
+    password
+):
+
+    return generate_password_hash(
+        password
+    )
+
+
+# ==========================================
+# CREAR ADMINISTRADOR
 # ==========================================
 def crear_admin_default():
 
+    usuario_admin = "admin"
+
     existe = obtener_usuario_por_username(
-        "admin"
+        usuario_admin
     )
 
     if existe:
 
         return
 
-    password_hash = generate_password_hash(
-        os.getenv(
-            "ADMIN_PASSWORD",
-            "7823"
-        )
+    password_admin = os.getenv(
+        "ADMIN_PASSWORD",
+        "7823"
+    )
+
+    password_hash = generar_hash_password(
+        password_admin
     )
 
     crear_usuario(
-
-        "admin",
-
+        usuario_admin,
         password_hash,
-
         "Administrador"
     )
